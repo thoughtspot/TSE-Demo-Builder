6a4d6809d61a65b8c463a65deb0f295c2d95b48a
Nathan Schroeder <nathan.schroeder@REM-NathanS.local>
2023-01-13 11:54:56 -0500

    on image viz beta

--- src/settings/Settings.js
+++ src/settings/Settings.js
@@ -467,6 +467,7 @@ function Link(props){
         <option value="Advanced">Custom Demo: Search String Builder</option>
         <option value="Custom Demos">Custom Demo: Tabbed Widgets</option>
         <option value="Survey Demo">Custom Demo: Survey Demo</option>
+        <option value="OnImageViz">OnImageViz</option>
 
       </select>
       {contentInput}
